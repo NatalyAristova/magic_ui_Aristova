e747e378ee22d73db7ace48ab242595ce04fb174
Aristovanatalia87@yandex.ru <Aristovanatalia87@yandex.ru>
2026-07-14 15:04:24 +0300

    Added ini

--- tests/item_page_test.py
+++ tests/item_page_test.py
@@ -1,14 +1,19 @@
+import pytest
+
+@pytest.mark.smoke
 def test_breadcrumbs(item_page):
     item_page.open_page()
     item_page.check_breadcrumbs()
 
 
+@pytest.mark.regression
 def test_add_item(item_page):
     item_page.open_page()
     item_page.add_item()
     item_page.assert_item_amount(2)
 
 
+@pytest.mark.extended
 def test_remove_item(item_page):
     item_page.open_page()
     item_page.remove_item()
